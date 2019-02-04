b9fdab423fb89c027ecf4508964f60ee770a7b6f
Aaron Stannard <aaron@petabridge.com>
2019-02-04 15:59:45 -0600

    copied over original WebCrawler code

---
+++ src/WebCrawler.CrawlService/CrawlerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Bootstrap.Docker;
+using WebCrawler.Shared.Config;
+
+namespace WebCrawler.CrawlService
+{
+    public class CrawlerService
+    {
+        protected ActorSystem ClusterSystem;
+
+        public Task WhenTerminated => ClusterSystem.WhenTerminated;
+
+
+        public bool Start()
+        {
+            var config = HoconLoader.ParseConfig("crawler.hocon");
+            ClusterSystem = ActorSystem.Create("webcrawler", config.BootstrapFromDocker());
+            return true;
+        }
+
+        public Task Stop()
+        {
+            return CoordinatedShutdown.Get(ClusterSystem).Run(CoordinatedShutdown.ClrExitReason.Instance);
+        }
+    }
+}
