520d1ff1819acd30026cb71b19a1579e58a2d43b
Aaron Stannard <aaron@petabridge.com>
2019-02-04 16:02:36 -0600

    added Lighthouse code

---
+++ src/Lighthouse/get-dockerip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$CLUSTER_IP" ]; then
+	host=$(hostname -i)
+	echo "Docker container bound on $host"
+	export CLUSTER_IP="$host"
+else
+	echo "Docker container bound on $CLUSTER_IP"
+fi
+
+exec "$@"
