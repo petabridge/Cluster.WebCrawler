7d6d127fb4fb975f9211dad20e8e104a22425b90
Aaron Stannard <aaron@petabridge.com>
2019-02-07 15:29:11 -0600

    added references to WebCrawler.Shared.DevOps in all projects

--- src/WebCrawler.Shared.DevOps/CrawlerBootstrapper.cs
+++ src/WebCrawler.Shared.DevOps/CrawlerBootstrapper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using Akka.Actor;
 using Akka.Bootstrap.Docker;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Host;
 using WebCrawler.Shared.DevOps.Config;
 
 namespace WebCrawler.Shared.DevOps
@@ -14,6 +17,19 @@ namespace WebCrawler.Shared.DevOps
         {
             var nextConfig = previousConfig.BootstrapFromDocker();
             return OpsConfig.GetOpsConfig().WithFallback(nextConfig);
+        }
+
+        /// <summary>
+        /// Start Petabridge.Cmd 
+        /// </summary>
+        /// <param name="system">The <see cref="ActorSystem"/> that will run Petabridge.Cmd</param>
+        /// <returns>The same <see cref="ActorSystem"/></returns>
+        public static ActorSystem StartPbm(this ActorSystem system)
+        {
+            var pbm = PetabridgeCmd.Get(system);
+            pbm.RegisterCommandPalette(ClusterCommands.Instance); // enable cluster management commands
+            pbm.Start();
+            return system;
         }
     }
 }
