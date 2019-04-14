78eb26140c76751be5a1d52a11b98491185e471c
Aaron Stannard <aaron@petabridge.com>
2019-04-14 11:17:12 -0500

    Include version number in Web UI (#25)
    * expose version number in Web UI
    
    * Updated common.props

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,2 @@
-#### 0.2.4 March 06 2019 ####
+#### 0.2.4 April 14 2019 ####
 * Upgraded to [Petabridge.Cmd v0.5.0](https://cmd.petabridge.com/articles/RELEASE_NOTES.html#v050-march-05-2019) so we could take advantage of the `cluster tail` command.
--- src/WebCrawler.Web/Controllers/HomeController.cs
+++ src/WebCrawler.Web/Controllers/HomeController.cs
@@ -8,8 +8,11 @@ namespace WebCrawler.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly string Version = typeof(HomeController).Assembly.GetName().Version.ToString();
+
         public IActionResult Index()
         {
+            ViewBag.AppVersion = Version;
             return View();
         }
     }
